d9a921605da8dbb7242dde11f8ee083047ec2fb8
ktroisn <brouillonquentin@gmail.com>
2023-06-28 16:46:30 +0200

    initial commit

---
+++ model/entities/User.php
@@ -0,0 +1,120 @@
+<?php
+    namespace Model\Entities;
+
+    use App\Entity;
+
+    final class User extends Entity{
+
+        private $id;
+        private $username;
+        private $password;
+        private $registration_date;
+        private $role;
+       
+
+        public function __construct($data){         
+            $this->hydrate($data);        
+        }
+ 
+        /**
+         * Get the value of id
+         */ 
+        public function getId()
+        {
+                return $this->id;
+        }
+
+        /**
+         * Set the value of id
+         *
+         * @return  self
+         */ 
+        public function setId($id)
+        {
+                $this->id = $id;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of username
+         */ 
+        public function getUsername()
+        {
+                return $this->username;
+        }
+
+        /**
+         * Set the value of id
+         *
+         * @return  self
+         */ 
+        public function setUsername($username)
+        {
+                $this->username = $username;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of password
+         */ 
+        public function getPassword()
+        {
+                return $this->password;
+        }
+
+        /**
+         * Set the value of password
+         *
+         * @return  self
+         */ 
+        public function setPassword($password)
+        {
+                $this->password = $password;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of registration
+         */ 
+        public function getRegistration()
+        {
+                return $this->registration_date;
+        }
+
+        /**
+         * Set the value of registration
+         *
+         * @return  self
+         */ 
+        public function setRegistration($registration_date)
+        {
+                $this->registration_date = $registration_date;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of role
+         */ 
+        public function getRole()
+        {
+                return $this->role;
+        }
+
+        /**
+         * Set the value of role
+         *
+         * @return  self
+         */ 
+        public function setRole($role)
+        {
+                $this->role = $role;
+
+                return $this;
+        }
+
+
+    }
